8e387209b9234e5c574471ecdf534cea4b6292d2
tommyntnu <99393113+tommyntnu@users.noreply.github.com>
2023-11-06 11:19:24 +0800

    Update index_chat.py

--- index_chat.py
+++ index_chat.py
@@ -34,7 +34,7 @@ def aoai_chat_model(chat):
         engine="DOE-GPT4",
         messages=recent_messages,
         temperature=0.7,
-        max_tokens=150,
+        max_tokens=250,
         top_p=0.9,
         frequency_penalty=0,
         presence_penalty=0,
